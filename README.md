9a2de95e790fdd6fc7552c07f686a14f7ffe7051
Marisa Ramon <mer0522@gmail.com>
2019-06-23 17:56:40 -0500

    Fix table display issue and update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Train-Scheduler
+
+#### To view project, clone or download to your local computer.
+
+#### This project utilizes Firebase to store data regarding train schedules. Allows admins to add new trains to the table, and it calculates the arrival time of the next train and the minutes away value.
+
+#### Tech used: Javascript, jQuery, Firebase, Moment.js
--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,7 @@
-// Initialize Firebase and change the values of the config values with your own Firebase config values.
+// Issues:
+// input form not taking military time
+// not sure if arrival time is correct
+
 var config = {
     apiKey: "AIzaSyCep-XJFBsDYDbtvp-LnmariNbFWtp8GHc",
     authDomain: "today-s-project-d97c6.firebaseapp.com",
@@ -11,11 +14,8 @@ var config = {
 
   firebase.initializeApp(config);
 
-  // Create a variable to reference the database
   var database = firebase.database();
 
-  // Initial Variables (SET the first set IN FIREBASE FIRST)
-  // Note remember to create these same variables in Firebase!
   var name = "";
   var destination = "";
   var firstTrainTime = 0;
@@ -29,21 +29,77 @@ $('.submit-button').on("click", function (event) {
     firstTrainTime = $("#first-train-time").val();
     frequency = $("#frequency").val();
 
+    var tBody = $("#table-body");
+    var tRow = $("<tr>");
+    var tdName = $("<td>").text(name);
+    var tdDestination = $("<td>").text(destination);
+    var tdFrequency = $("<td>").text(frequency);
+    var tdArrival = $("<td>").text(tArrival);
+    var tdMinutes = $("<td>").text(tMinutes);
+    tRow.append(tdName, tdDestination, tdFrequency, tdArrival, tdMinutes);
+    tBody.append(tRow);
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train-time").val("");
+    $("#frequency").val("");
 
-    // newRow = $("<tr>");
-     $("#new-table-row").append("<td>" + name + "</td>");
-    $("#new-table-row").append("<td>" + destination + "</td>");
-    $("#new-table-row").append("<td>" + frequency + "</td>");
+
 
     // Change what is saved in firebase
-    // database.ref().set({
-    //     name: name,
-    //     destination: destination,
-    //     firstTrainTime: firstTrainTime,
-    //     frequency: frequency
-    // });
-
-
-
-
+    database.ref().push({
+        name: name,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency
+    });
 })
+
+// Firebase is always watching for changes to the data.
+    // When changes occurs it will print them to console and html
+    database.ref().on("value", function(snapshot) {
+
+        // Print the initial data to the console.
+        console.log(snapshot.val());
+  
+        // Log the value of the various properties
+        console.log(snapshot.val().name);
+        console.log(snapshot.val().destination);
+        console.log(snapshot.val().firstTrainTime);
+        console.log(snapshot.val().frequency);
+
+        // First time split into array
+        var timeArr = snapshot.val().firstTrainTime.split(":");
+        console.log(timeArr);
+        var trainTime = moment().hours(timeArr[0]);
+        console.log("trainTime: " + trainTime.format("hh:mm"));
+        var maxMoment = moment.max(moment(),trainTime);
+        console.log("maxMoment: ", + maxMoment);
+
+        if (maxMoment === trainTime){
+            tArrival = trainTime.format("hh:mm");
+            console.log("tArrival"+ tArrival);
+            tMinutes = trainTime.diff(moment(), "minutes");
+            console.log("tMinutes:", + tMinutes);
+        }
+        else { 
+            // calculate difference between the times
+            var differenceTimes = moment().diff(trainTime, "minutes");
+            // time apart (remainder)
+            var tRemainder = differenceTimes % snapshot.val().frequency;
+            // minutes until next train
+            tMinutes = snapshot.val().frequency - tRemainder;
+            // next train arrival time
+            tArrival = moment().add(tMinutes, "m").format("hh:mm");
+        }
+
+        console.log("tMinutes: " + tMinutes);
+        console.log("tArrival: "+ tArrival);
+  
+        // Change the HTML
+        // (moved this)
+  
+        // If any errors are experienced, log them to console.
+      }, function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+
